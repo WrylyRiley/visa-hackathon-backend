771cbbabda5ba4339a92b51522fcf27c239f5a6a
Zach Bauer <zbauer91@gmail.com>
2018-04-28 10:06:20 -0400

    sends formatted response to slack from mock_data

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ app.get('/api/v1/healthz', (req, res) => {
   res.send('life ok mon')
 })
 
-app.get('/api/v1/invoices', (req, res) => {
+app.post('/api/v1/invoices', (req, res) => {
   res.json(mockData)
+})
+
+app.post('/api/v1/rabbit', (req, res) => {
+  let attachments = []
+  mockData.forEach(element => {
+    attachments.push({
+      title: `Balance Due: ${element.balance_due}`,
+      pretext: `*${element.vendor_name}*`,
+      text: `Invoice Date: ${element.invoice_date}\nDue Date: ${element.due_date}`,
+      mrkdwn_in: ['pretext']
+    })
+  })
+  res.json({ attachments: attachments })
 })
