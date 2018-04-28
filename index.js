ff9a39aa6ba8aa5aaa215f0014ae7cdf0e0f1f2a
Zach Bauer <zbauer91@gmail.com>
2018-04-27 21:46:24 -0400

    rearranges api endpoints

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = require('express')()
 
-app.get('/api/v1/healthz', (req, res) => {
-  res.send('life is a ok')
-})
 app.listen(4000, () => {
   console.log('app listening on port 4000')
+})
+
+app.get('/api/v1/healthz', (req, res) => {
+  res.send('life is a ok')
 })
