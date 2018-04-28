e14343bc11f87ac32ff617a43cce77bc503a0220
Zach Bauer <zbauer91@gmail.com>
2018-04-27 22:08:50 -0400

    adds route for mock data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require('express')()
+const mockData = require('./MOCK_DATA.json')
 
 app.listen(4000, () => {
   console.log('app listening on port 4000')
@@ -6,4 +7,8 @@ app.listen(4000, () => {
 
 app.get('/api/v1/healthz', (req, res) => {
   res.send('life is a ok')
+})
+
+app.get('/api/v1/invoices', (req, res) => {
+  res.json(mockData)
 })
